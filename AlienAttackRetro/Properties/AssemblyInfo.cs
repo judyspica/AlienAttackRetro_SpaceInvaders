c20102fef79f61158ea44c2b75a1e301e1d668db
Judy <61878992+judyspica@users.noreply.github.com>
2022-02-10 14:50:32 +0100

    Update AssemblyInfo.cs

--- AlienAttackRetro/Properties/AssemblyInfo.cs
+++ AlienAttackRetro/Properties/AssemblyInfo.cs
@@ -1,10 +1,3 @@
-﻿// -----------------------------------------------------------------------------
-// <copyright file="AssemblyInfo.cs" company="FHWN">
-// Copyright (c) FHWN. All rights reserved.
-// </copyright>
-// <summary>This program is a crosshair shooting aliens game.</summary>
-// <author>Judy Kardouh.</author>
-// -----------------------------------------------------------------------------
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
